106dd911dda087aff55bdbc4e1c1422dbb63f4e9
malasen <malasenkan.mu@gmail.com>
2023-04-18 10:53:52 +0200

    feat: Fetching Images from Api

--- artirate-app/src/routes/Home.jsx
+++ artirate-app/src/routes/Home.jsx
@@ -1,11 +1,24 @@
+import React from "react";
 import { useContext } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
+import axios from "axios";
+import { API_ImgUrl } from "../constants/api.js";
 import AuthContext from "../services/AuthContext.js";
-import { logOut } from "../services/loginServices.js";
 import Login from "../components/login/Login.jsx";
 
 const Home = () => {
     const { user } = useContext(AuthContext);
+
+    const [post, setPost] = React.useState(null);
+    
+  React.useEffect(() => {
+    axios.get(API_ImgUrl).then((response) => {
+      setPost(response.data);
+      console.log(response.data)
+    });
+  }, []);
+
+  if (!post) return null;
 
     if (!user) {
         return <>
@@ -15,6 +28,9 @@ const Home = () => {
     return (
     <>
     <div>Home page with Images</div>
+    {post.map(p => (
+        <div>{p.imgTitle}</div>
+    ))}
     </>
     );
 };
